ebe92f377e7cf6e52d8994731f2e3def404fbadf
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2026-05-05 21:03:10 +0300

    Add LazyProperty, Math classes
    Add __main__

--- lazy_property.py
+++ lazy_property.py
@@ -16,3 +16,34 @@ def fib(n):
 @pytest.mark.parametrize("n, res", zip(range(10), [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]))
 def test_fib(n, res):
     assert fib(n) == res
+
+
+class LazyProperty:
+    def __init__(self, func, arg):
+        self._func = func
+        self._arg = arg
+
+    def __set_name__(self, owner, name):
+        self._name = name
+
+    def __get__(self, instance, owner=None):
+        if instance is None:
+            return self
+        if self._name in instance.__dict__:
+            return instance.__dict__[self._name]
+        else:
+            val = self._func(self._arg)
+            instance.__dict__[self._name] = val
+            return val
+
+    def __set__(self, instance, value):
+        pass
+
+
+class Math:
+    fib = LazyProperty(fib, 8)
+
+
+if __name__ == "__main__":
+    m = Math()
+    print(m.fib)
